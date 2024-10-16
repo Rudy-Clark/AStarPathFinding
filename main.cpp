10d2e409e69c98147de295d8a8e3be53a2dc7166
Rudy Clark <rudy.uzb@gmail.com>
2024-10-16 10:25:30 +0500

    convert vector to raw c style list.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <float.h>
 #include <math.h>
 #include <raylib.h>
+#include <raymath.h>
 #include <string>
 #include <vector>
 
 #include "AStar.hpp"
 #include "Commands.hpp"
 
-#define FLT_STRING_SIZE (1+1+1+(FLT_DECIMAL_DIG-1)+1+1+ 4   +1)
+#define FLT_STRING_SIZE (1+1+1+(FLT_DECIMAL_DIG-1)+1+1+4+1)
 #define SIZE 256
 
 // TODO 
@@ -38,19 +39,38 @@ void genObstacles(Vector2* obstacles, int numCells) {
     }
 }
 
+void setSolutionPath(Vector2* solutionPath, const AStar::CoordinateList& path) {
+    for (int i{ 0 }; i < path.size() % SIZE; ++i) {
+        solutionPath[i] = { (float)path[i].x, (float)path[i].y };
+   }
+}
+
 int main() {
+    Commands comm;
+
     InitWindow(Settings::screenWidth, Settings::screenHeight, "Window");
     SetTargetFPS(GetMonitorRefreshRate(0));
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT | FLAG_WINDOW_HIGHDPI);
+    Vector2 mousePos = { -1.0f, -1.0f };
+    Vector2 solutionPath[SIZE];
+
     Shader gridShader = LoadShader("./Shaders/grid.vert", "./Shaders/grid.frag");
-    //Shader gridShader = LoadShaderFromMemory(gridShaderVertexCode, gridShaderFragmentCode);
+
     // Set up uniform locations
     int screenSizeLoc = GetShaderLocation(gridShader, "screenSize");
     int gridSpacingLoc = GetShaderLocation(gridShader, "gridSpacing");
     int gridColorLoc = GetShaderLocation(gridShader, "gridColor");
-    int activeCellLoc = GetShaderLocation(gridShader, "u_activeCell");
-    int activeCellColorLoc = GetShaderLocation(gridShader, "u_activeCellColor");
-    
+    int startCellLoc = GetShaderLocation(gridShader, "u_startCell");
+    int startCellColorLoc = GetShaderLocation(gridShader, "u_startCellColor");
+    int endCellLoc = GetShaderLocation(gridShader, "u_endCell");
+    int endCellColorLoc = GetShaderLocation(gridShader, "u_endCellColor");
+    int solutionPathLoc = GetShaderLocation(gridShader, "u_solutionPath");
+    int solutionPathSizeLoc = GetShaderLocation(gridShader, "u_solutionPathSize");
+    int solutionPathColorLoc = GetShaderLocation(gridShader, "u_solutionPathColor");
+
+    Vector3 endCellColor = { 1.f, 0.f, 0.f };
+    SetShaderValue(gridShader, endCellColorLoc, &endCellColor, SHADER_UNIFORM_VEC3);
+
     // obstacles
     int obstacleListLoc = GetShaderLocation(gridShader, "ua_obstacles");
     // obstacle color
@@ -68,6 +88,9 @@ int main() {
     // obstacle color;
     Vector3 ObstacleColor = {0.51f, 0.51f, 0.51f};
     SetShaderValue(gridShader, obstacleColorLoc, &ObstacleColor, SHADER_UNIFORM_VEC3);
+    // solution path color
+    Vector3 solutionPathColor = { 0.f, 0.f, 1.f };
+    SetShaderValue(gridShader, solutionPathColorLoc, &solutionPathColor, SHADER_UNIFORM_VEC3);
 
     const int numCells = (GetScreenWidth() / gridSpacing);
     
@@ -77,23 +100,20 @@ int main() {
     // generate obstacles
     genObstacles(obstaclesRow, numCells);
     AStar::Generator generator;
-    generator.setDiagonalMovement(false);
-    generator.setWorldSize({ numCells, numCells });
-    // define collision cells
-    for (int i{ 0 }; i < SIZE; ++i) {
-        generator.addCollision({ (int)obstaclesRow[i].x, (int)obstaclesRow[i].y });
-    }
+    AStar::CoordinateList path;
+    int pathSize{ 0 };
 
-    Commands comm;
-    comm.Push({ 1.f, 1.f });
+    generator.setDiagonalMovement(false);
+    generator.setHeuristic(AStar::Heuristic::euclidean);
+    generator.setWorldSize({ numCells, numCells });
 
     int obstaclesRowLoc = GetShaderLocation(gridShader, "u_obstacles");
     SetShaderValueV(gridShader, obstaclesRowLoc, obstaclesRow, SHADER_UNIFORM_VEC2, SIZE);
 
-    Vector2 mousePos = { 0.0, 0.0 };
-    SetShaderValue(gridShader, activeCellLoc, &mousePos, SHADER_UNIFORM_VEC2);
-    Vector3 activeCellColor = { 0.f, 228.f / 255.f, 48.f / 255.f };
-    SetShaderValue(gridShader, activeCellColorLoc, &activeCellColor, SHADER_UNIFORM_VEC3);
+    SetShaderValue(gridShader, startCellLoc, &mousePos, SHADER_UNIFORM_VEC2);
+    SetShaderValue(gridShader, endCellLoc, &mousePos, SHADER_UNIFORM_VEC2);
+    Vector3 startCellColor = { 0.f, 228.f / 255.f, 48.f / 255.f };
+    SetShaderValue(gridShader, startCellColorLoc, &startCellColor, SHADER_UNIFORM_VEC3);
 
     std::string gridSizeText = "Grid Space: " + std::to_string((int)gridSpacing);
     std::string posText = "Vec2(" + std::to_string((int)mousePos.x) + ", " + std::to_string((int)mousePos.y) + ")";
@@ -105,10 +125,10 @@ int main() {
         // set grid cell active by click mouse left button
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             mousePos = { std::floor(GetMousePosition().x / gridSpacing), std::floor((GetScreenHeight() - GetMousePosition().y) / gridSpacing) };
-            SetShaderValue(gridShader, activeCellLoc, &mousePos, SHADER_UNIFORM_VEC2);
-            posText = "Vec2(" + std::to_string((int)mousePos.x) + ", " + std::to_string((int)mousePos.y) + ")";
             comm.Push(mousePos);
-        }
+            SetShaderValue(gridShader, startCellLoc, &comm.GetStart(), SHADER_UNIFORM_VEC2);
+            SetShaderValue(gridShader, endCellLoc, &comm.GetEnd(), SHADER_UNIFORM_VEC2);
+            posText = "Vec2(" + std::to_string((int)mousePos.x) + ", " + std::to_string((int)mousePos.y) + ")";        }
 
         if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
             comm.ClearCommands();
@@ -119,7 +139,12 @@ int main() {
                 for (int i{ 0 }; i < SIZE; ++i) {
                     generator.addCollision({ (int)obstaclesRow[i].x, (int)obstaclesRow[i].y });
                 }
-                generator.findPath({ (int)comm.GetStart().x,  (int)comm.GetStart().y }, { (int)comm.GetEnd().x,  (int)comm.GetEnd().y });
+                path = generator.findPath({ (int)comm.GetStart().x,  (int)comm.GetStart().y }, { (int)comm.GetEnd().x,  (int)comm.GetEnd().y });
+                pathSize = path.size();
+                setSolutionPath(solutionPath, path);
+                SetShaderValue(gridShader, solutionPathSizeLoc, &pathSize, SHADER_UNIFORM_INT);
+                SetShaderValueV(gridShader, solutionPathLoc, solutionPath, SHADER_UNIFORM_VEC2, path.size() % SIZE);
+                generator.clearCollisions();
             }
         }
 
@@ -136,7 +161,6 @@ int main() {
         }*/
 
         if (IsKeyPressed(KEY_R)) {
-            generator.clearCollisions();
             genObstacles(obstaclesRow, numCells);
             SetShaderValueV(gridShader, obstaclesRowLoc, obstaclesRow, SHADER_UNIFORM_VEC2, SIZE);
         }
@@ -146,6 +170,7 @@ int main() {
         EndShaderMode();
 
         DrawText(gridSizeText.c_str(), GetScreenWidth() - 200, 8, 28, RED);
+
         DrawText(posText.c_str(), GetScreenWidth() - 150, 48, 16, RED);
         DrawFPS(14, 8);
         EndDrawing();
